2520edb37575244868d5a9e1234fe8e13a331536
zaimoni <tharichizi@gmail.com>
2018-07-11 02:32:59 -0500

    eliminate the setvector variant for pl_flags

--- setvector.h
+++ setvector.h
@@ -6,13 +6,16 @@
 #include "mapitems.h"
 #include "crafting.h"
 #include "mission.h"
+
+// Zaimoni, 2018-07-10: looks like all of the MSVC-breaking setvector calls 
+// were replaced by externally loaded configuration in C:DDA prior to the Coolthulu fork.
+// no direct guidance for immediately bringing up the build.
 void setvector(std::vector <itype_id> &vec, ... );
 void setvector(std::vector <component> &vec, ... );
 void setvector(std::vector <mon_id> &vec, ... );
 void setvector(std::vector <items_location_and_chance> &vec, ... );
 void setvector(std::vector <mission_origin> &vec, ... );
 void setvector(std::vector <std::string> &vec, ... );
-void setvector(std::vector <pl_flag> &vec, ... );
 void setvector(std::vector <m_flag> &vec, ... );
 void setvector(std::vector <monster_trigger> &vec, ... );
 void setvector(std::vector <moncat_id> &vec, ... );
