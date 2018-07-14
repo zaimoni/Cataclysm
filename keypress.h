c8815b9d66ad683dc53ae758b32a7788d89e54a3
zaimoni <tharichizi@gmail.com>
2018-07-14 09:01:06 -0500

    call graph: default_keymap_txt

--- keypress.h
+++ keypress.h
@@ -1,8 +1,6 @@
 #ifndef _KEYPRESS_H_
 #define _KEYPRESS_H_
 
-#include <string>
-
 class game;
 
 // Simple text input--translates numpad to vikeys
@@ -11,7 +9,6 @@ long input();
 void get_direction(int &x, int &y, char ch);
 // Uses the keymap to figure out direction properly
 void get_direction(game *g, int &x, int &y, char ch);
-std::string default_keymap_txt();
 
 #define CTRL(n) (n - 'A' + 1 < 1 ? n - 'a' + 1 : n - 'A' + 1)
 #define KEY_ESCAPE 27
