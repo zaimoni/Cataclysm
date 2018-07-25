d75ad2729ed508e81ebca1a7439a797f025f3824
zaimoni <tharichizi@gmail.com>
2018-07-25 04:01:58 -0500

    correctness/dead code removal adjustments

--- Zaimoni.STL/Compiler.h
+++ Zaimoni.STL/Compiler.h
@@ -1,6 +1,6 @@
 /* Compiler.h */
 /* cross-compiler compatiblity header */
-/* (C)2009,2010 Kenneth Boyd, license: MIT.txt */
+/* (C)2009,2010,2018 Kenneth Boyd, license: MIT.txt */
 
 #ifndef ZAIMONI_COMPILER_H
 #define ZAIMONI_COMPILER_H 1
@@ -28,6 +28,12 @@
 #define ZAIMONI_ISO_ONLY(A) A
 #endif
 
+/* delayed expansion of preprocessing operators */
+#define STRINGIZE(A) #A
+#define DEEP_STRINGIZE(A) STRINGIZE(A)
+#define CONCATENATE(A,B) A ## B
+#define DEEP_CONCATENATE(A,B) CONCATENATE(A,B)
+
 /* hooks for C/C++ adapter code */
 #ifdef __cplusplus
 #define EXTERN_C extern "C"
@@ -35,33 +41,14 @@
 #define EXTERN_C extern
 #endif
 
-/* hooks for compiler extensions
-   CONST_C_FUNC: like PURE_C_FUNC, but also cannot access global memory (through pointers or global variables)
-   PURE_C_FUNC: no effects except return value, return value depends only on parameters and global variables
-   PTR_RESTRICT: C99 restrict semantics (pointer may be assumed not to be aliased) */
+/* hooks for compiler extensions (this could be part of compiler test results) */
 #define NO_RETURN
 #define MS_NO_RETURN
-#define CONST_C_FUNC
-#define PURE_C_FUNC
-#define ALL_PTR_NONNULL
-#define PTR_NONNULL(A)
-#define PTR_RESTRICT
 
 /* GCC */
 #ifdef __GNUC__
-#define THROW_COMPETENT 1
 #undef NO_RETURN
-#undef CONST_C_FUNC
-#undef PURE_C_FUNC
-#undef ALL_PTR_NONNULL
-#undef PTR_NONNULL
-#undef PTR_RESTRICT
 #define NO_RETURN __attribute__ ((noreturn))
-#define CONST_C_FUNC __attribute__ ((const))
-#define PURE_C_FUNC __attribute__ ((pure))
-#define ALL_PTR_NONNULL __attribute__ ((nonnull))
-#define PTR_NONNULL(A) __attribute__ ((nonnull A))
-#define PTR_RESTRICT __restrict__
 #endif
 #ifdef _MSC_VER
 #undef MS_NO_RETURN
