1f4b1f8ef267a14963af7d1c05c8f142a9373ca3
zaimoni <tharichizi@gmail.com>
2020-11-04 20:35:47 -0600

    make inventory screens respond to configured dimensions

--- inventory_ui.cpp
+++ inventory_ui.cpp
@@ -14,39 +14,42 @@ static void print_inv_statics(game *g, WINDOW* w_inv, std::string title, const s
 // Print our header
  mvwprintw(w_inv, 0, 0, title.c_str());
 
+ const int mid_pt = SCREEN_WIDTH / 2;
+ const int three_quarters = 3 * SCREEN_WIDTH / 4;
+
 // Print weight
- mvwprintw(w_inv, 0, 40, "Weight: ");
+ mvwprintw(w_inv, 0, mid_pt, "Weight: ");
  const int my_w_capacity = g->u.weight_capacity();
  const int my_w_carried = g->u.weight_carried();
  wprintz(w_inv, (my_w_carried >= my_w_capacity / 4) ? c_red : c_ltgray, "%d", my_w_carried);
  wprintz(w_inv, c_ltgray, "/%d/%d", my_w_capacity/4, my_w_capacity);
 
 // Print volume
- mvwprintw(w_inv, 0, 60, "Volume: ");
+ mvwprintw(w_inv, 0, three_quarters, "Volume: ");
  const int my_v_capacity = g->u.volume_capacity() - 2;
  const int my_v_carried = g->u.volume_carried();
  wprintz(w_inv, (my_v_carried > my_v_capacity) ? c_red : c_ltgray, "%d", my_v_carried);
  wprintw(w_inv, "/%d", my_v_capacity);
 
 // Print our weapon
- mvwprintz(w_inv, 2, 40, c_magenta, "WEAPON:");
+ mvwprintz(w_inv, 2, mid_pt, c_magenta, "WEAPON:");
  if (g->u.is_armed()) {
   const bool dropping_weapon = cataclysm::any(dropped_items, g->u.weapon.invlet);
   if (dropping_weapon)
-   mvwprintz(w_inv, 3, 40, c_white, "%c + %s", g->u.weapon.invlet, g->u.weapname().c_str());
+   mvwprintz(w_inv, 3, mid_pt, c_white, "%c + %s", g->u.weapon.invlet, g->u.weapname().c_str());
   else
-   mvwprintz(w_inv, 3, 40, g->u.weapon.color_in_inventory(g->u), "%c - %s", g->u.weapon.invlet, g->u.weapname().c_str());
+   mvwprintz(w_inv, 3, mid_pt, g->u.weapon.color_in_inventory(g->u), "%c - %s", g->u.weapon.invlet, g->u.weapname().c_str());
  } else if (g->u.weapon.is_style())
-  mvwprintz(w_inv, 3, 40, c_ltgray, "%c - %s", g->u.weapon.invlet, g->u.weapname().c_str());
+  mvwprintz(w_inv, 3, mid_pt, c_ltgray, "%c - %s", g->u.weapon.invlet, g->u.weapname().c_str());
  else
-  mvwprintz(w_inv, 3, 42, c_ltgray, g->u.weapname().c_str());
+  mvwprintz(w_inv, 3, mid_pt + 2, c_ltgray, g->u.weapname().c_str());
 // Print worn items
  if (!g->u.worn.empty()) {
-     mvwprintz(w_inv, 5, 40, c_magenta, "ITEMS WORN:");
+     mvwprintz(w_inv, 5, mid_pt, c_magenta, "ITEMS WORN:");
      int row = 6;
      for (const item& armor : g->u.worn) {
          const bool dropping_armor = cataclysm::any(dropped_items, armor.invlet);
-         mvwprintz(w_inv, row++, 40, (dropping_armor ? c_white : c_ltgray), "%c + %s", armor.invlet, armor.tname().c_str());
+         mvwprintz(w_inv, row++, mid_pt, (dropping_armor ? c_white : c_ltgray), "%c + %s", armor.invlet, armor.tname().c_str());
      }
  }
 }
@@ -84,7 +87,7 @@ static auto find_firsts(const inventory &inv)
 char game::inv(std::string title)
 {
  static const std::vector<char> null_vector;
- constexpr const int maxitems = 20;	// Number of items to show at one time.
+ const int maxitems = VIEW - 5;	// Number of items to show at one time.
  int ch = '.';
  int start = 0, cur_it;
  u.sort_inv();
@@ -96,8 +99,7 @@ char game::inv(std::string title)
 
  do {
   if (ch == '<' && start > 0) { // Clear lines and shift
-   for (int i = 1; i < VIEW; i++)
-    mvwprintz(w_inv.get(), i, 0, c_black, "                                        ");
+   for (int i = 1; i < VIEW; i++) draw_hline(w_inv.get(), i, c_black, ' ', 0, SCREEN_WIDTH / 2);
    start -= maxitems;
    if (start < 0) start = 0;
    mvwprintw(w_inv.get(), maxitems + 2, 0, "         ");
@@ -105,8 +107,7 @@ char game::inv(std::string title)
   if (ch == '>' && cur_it < u.inv.size()) { // Clear lines and shift
    start = cur_it;
    mvwprintw(w_inv.get(), maxitems + 2, 12, "            ");
-   for (int i = 1; i < VIEW; i++)
-    mvwprintz(w_inv.get(), i, 0, c_black, "                                        ");
+   for (int i = 1; i < VIEW; i++) draw_hline(w_inv.get(), i, c_black, ' ', 0, SCREEN_WIDTH / 2);
   }
   int cur_line = 2;
   for (cur_it = start; cur_it < start + maxitems && cur_line < 23; cur_it++) {
@@ -120,16 +121,15 @@ char game::inv(std::string title)
     }
    }
    if (cur_it < u.inv.size()) {
-    mvwputch (w_inv.get(), cur_line, 0, c_white, u.inv[cur_it].invlet);
-    mvwprintz(w_inv.get(), cur_line, 1, u.inv[cur_it].color_in_inventory(u), " %s",
-              u.inv[cur_it].tname().c_str());
+    const item& it = u.inv[cur_it];
+    mvwputch (w_inv.get(), cur_line, 0, c_white, it.invlet);
+    mvwprintz(w_inv.get(), cur_line, 1, it.color_in_inventory(u), " %s", it.tname().c_str());
     if (u.inv.stack_at(cur_it).size() > 1)
      wprintw(w_inv.get(), " [%d]", u.inv.stack_at(cur_it).size());
-    if (u.inv[cur_it].charges > 0)
-     wprintw(w_inv.get(), " (%d)", u.inv[cur_it].charges);
-    else if (u.inv[cur_it].contents.size() == 1 &&
-             u.inv[cur_it].contents[0].charges > 0)
-     wprintw(w_inv.get(), " (%d)", u.inv[cur_it].contents[0].charges);
+    if (it.charges > 0)
+     wprintw(w_inv.get(), " (%d)", it.charges);
+    else if (it.contents.size() == 1 && it.contents[0].charges > 0)
+     wprintw(w_inv.get(), " (%d)", it.contents[0].charges);
    }
    cur_line++;
   }
@@ -150,7 +150,7 @@ std::vector<item> game::multidrop()
  u.sort_inv();
  u.inv.restack(&u);
  WINDOW* w_inv = newwin(VIEW, SCREEN_WIDTH, 0, 0);
- const int maxitems = 20;    // Number of items to show at one time.
+ const int maxitems = VIEW - 5; // Number of items to show at one time.
  std::vector<int> dropping(u.inv.size(), 0);
  int count = 0; // The current count
  std::vector<char> weapon_and_armor; // Always single, not counted
@@ -164,8 +164,7 @@ std::vector<item> game::multidrop()
  size_t cur_it = 0;
  do {
   if (ch == '<' && start > 0) {
-   for (int i = 1; i < VIEW; i++)
-    mvwprintz(w_inv, i, 0, c_black, "                                        ");
+   for (int i = 1; i < VIEW; i++) draw_hline(w_inv, i, c_black, ' ', 0, SCREEN_WIDTH / 2);
    start -= maxitems;
    if (start < 0) start = 0;
    mvwprintw(w_inv, maxitems + 2, 0, "         ");
@@ -173,11 +172,10 @@ std::vector<item> game::multidrop()
   if (ch == '>' && cur_it < u.inv.size()) {
    start = cur_it;
    mvwprintw(w_inv, maxitems + 2, 12, "            ");
-   for (int i = 1; i < VIEW; i++)
-    mvwprintz(w_inv, i, 0, c_black, "                                        ");
+   for (int i = 1; i < VIEW; i++) draw_hline(w_inv, i, c_black, ' ', 0, SCREEN_WIDTH / 2);
   }
   int cur_line = 2;
-  for (cur_it = start; cur_it < start + maxitems && cur_line < 23; cur_it++) {
+  for (cur_it = start; cur_it < start + maxitems && cur_line < VIEW - 2; cur_it++) {
 // Clear the current line;
    mvwprintw(w_inv, cur_line, 0, "                                    ");
 // Print category header
@@ -188,21 +186,20 @@ std::vector<item> game::multidrop()
     }
    }
    if (cur_it < u.inv.size()) {
-    mvwputch (w_inv, cur_line, 0, c_white, u.inv[cur_it].invlet);
+    const item& it = u.inv[cur_it];
+    mvwputch (w_inv, cur_line, 0, c_white, it.invlet);
     char icon = '-';
     if (dropping[cur_it] >= u.inv.stack_at(cur_it).size())
      icon = '+';
     else if (dropping[cur_it] > 0)
      icon = '#';
     nc_color col = (dropping[cur_it] == 0 ? c_ltgray : c_white);
-    mvwprintz(w_inv, cur_line, 1, col, " %c %s", icon, u.inv[cur_it].tname().c_str());
+    mvwprintz(w_inv, cur_line, 1, col, " %c %s", icon, it.tname().c_str());
     if (u.inv.stack_at(cur_it).size() > 1)
      wprintz(w_inv, col, " [%d]", u.inv.stack_at(cur_it).size());
-    if (u.inv[cur_it].charges > 0)
-     wprintz(w_inv, col, " (%d)", u.inv[cur_it].charges);
-    else if (u.inv[cur_it].contents.size() == 1 &&
-             u.inv[cur_it].contents[0].charges > 0)
-     wprintw(w_inv, " (%d)", u.inv[cur_it].contents[0].charges);
+    if (it.charges > 0) wprintz(w_inv, col, " (%d)", it.charges);
+    else if (it.contents.size() == 1 && it.contents[0].charges > 0)
+     wprintw(w_inv, " (%d)", it.contents[0].charges);
    }
    cur_line++;
   }
@@ -226,8 +223,7 @@ std::vector<item> game::multidrop()
      }
     }
     if (!found) {
-     if (ch == u.weapon.invlet && u.weapon.type->id > num_items &&
-         u.weapon.type->id < num_all_items) {
+     if (ch == u.weapon.invlet && is_between(num_items + 1, u.weapon.type->id, num_all_items-1)) {
       if (!warned_about_bionic)
        messages.add("You cannot drop your %s.", u.weapon.tname().c_str());
       warned_about_bionic = true;
@@ -237,19 +233,15 @@ std::vector<item> game::multidrop()
      }
     }
    } else {
+    const size_t ub = u.inv.stack_at(index).size();
     if (count == 0) {
-     if (dropping[index] == 0)
-      dropping[index] = u.inv.stack_at(index).size();
-     else
-      dropping[index] = 0;
-    } else if (count >= u.inv.stack_at(index).size())
-     dropping[index] = u.inv.stack_at(index).size();
-    else
-     dropping[index] = count;
+     dropping[index] = (0 == dropping[index]) ? ub : 0;
+    } else {
+     dropping[index] = (count >= ub) ? ub : count;
+    }
    }
    count = 0;
   }
-   
  } while (ch != '\n' && ch != KEY_ESCAPE && ch != ' ');
  werase(w_inv);
  delwin(w_inv);
