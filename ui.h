7b20c2eabfaa21bf008d19a1b05ba9fa377877eb
zaimoni <tharichizi@gmail.com>
2020-11-29 02:41:32 -0600

    ARCHAIC_UI build option (allows testing 80x25 screen layout)

--- ui.h
+++ ui.h
@@ -1,8 +1,6 @@
 #ifndef UI_H
 #define UI_H 1
 
-// VAPORWARE: VIEW and PANELX are candidates for configuration options for screen size.
-
 // display constants related to map scaling/implementation and the curses console
 enum {
 	// SEEX/SEEY is how far the player can see in the X/Y direction (at least, without scrolling).
@@ -10,8 +8,13 @@ enum {
 	SEE = 12,	// for when it matters that they are explicitly equal
 	SEEX = SEE,
 	SEEY = SEE,
+#ifdef ARCHAIC_UI
 	VIEW = 2 * SEEX + 1,	// required curses view window height.  Default 25.  Fatcat graphical clients can resize as needed.
 	VIEW_CENTER = VIEW/2,	// not redundant once configuration options go in
+	PANELX = 55,
+	// the canonical view with the player-scale map had a 55-wide "control panel", for a total width of 25+55=80
+	SCREEN_WIDTH = VIEW + PANELX,
+#endif
 	// minimap
 	MINIMAP_WIDTH_HEIGHT = 7,
 	// status bar
@@ -19,17 +22,16 @@ enum {
 	// mon info
 	MON_INFO_HEIGHT = 12,
 	TABBED_HEADER_HEIGHT = 3,
-	VBAR_X = 30, // number of screens have hard-coded left pane 30 width
-	// the canonical view with the player-scale map had a 55-wide "control panel", for a total width of 25+55=80
-	PANELX = 55,
-	SCREEN_WIDTH = VIEW+PANELX
+	VBAR_X = 30 // number of screens have hard-coded left pane 30 width
 };
 
+#ifndef ARCHAIC_UI
 // Technical debt: macros dependent on option_table @ options.h
 #define VIEW ((int)(option_table::get()[OPT_VIEW]))
 #define VIEW_CENTER (VIEW/2)
 #define PANELX ((int)(option_table::get()[OPT_PANELX]))
 #define SCREEN_WIDTH ((int)(option_table::get()[OPT_SCREENWIDTH]))
+#endif
 
 #endif
 
