0d2346c914f9cf5c3df982e4b40b63a8002b11f6
zaimoni <tharichizi@gmail.com>
2021-01-12 11:32:12 -0600

    disconnect set_vector.h from the build

--- setvector.h
+++ setvector.h
@@ -4,6 +4,9 @@
 // Zaimoni, 2018-07-10: looks like all of the MSVC-breaking setvector calls 
 // were replaced by externally loaded configuration in C:DDA prior to the Coolthulu fork.
 // no direct guidance for immediately bringing up the build.
+
+// Eliminated outright. 2021-01-12 zaimoni
+// std::initializer_list is sufficient to handle anything these macros can handle.
 #define SET_VECTOR(DEST,...)	\
 {	\
 const auto tmp = { __VA_ARGS__ };	\
