29c335b931d4d3e67fa2b8998dc37c4fa362e89e
zaimoni <tharichizi@gmail.com>
2021-09-15 13:42:19 -0500

    unbreak build: pc.hpp

---
+++ pc.hpp
@@ -0,0 +1,21 @@
+#ifndef PC_HPP
+#define PC_HPP 1
+
+#include "player.h"
+
+// Ok to migrate per-player game UI here.
+class pc final : public player
+{
+public:
+	pc() = default;
+	pc(const cataclysm::JSON& src) : player(src) {}
+	pc(const pc& rhs) = default;
+	pc(pc&& rhs) = default;
+	virtual ~pc() = default;
+//	friend cataclysm::JSON toJSON(const player & src); // will need to adjust this eventually
+
+	pc& operator=(const pc& rhs);
+	pc& operator=(pc&& rhs) = default;
+};
+
+#endif
